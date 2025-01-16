7305aa0f1fbbdb92cecc99a19566f7c0e5d50131
Enrico <crs.enrico@gmail.com>
2025-01-17 00:41:25 +0100

    Typo

--- esphome/components/esp32_ble/ble.cpp
+++ esphome/components/esp32_ble/ble.cpp
@@ -208,9 +208,9 @@ bool ESP32BLE::ble_setup_() {
     }
   }
  
-  err = esp_ble_gap_set_prefered_default_phy(ESP_BLE_GAP_PHY_OPTIONS_PREF_S8_CODING,ESP_BLE_GAP_PHY_OPTIONS_PREF_S8_CODING);
+  err = esp_ble_gap_set_preferred_default_phy(ESP_BLE_GAP_PHY_OPTIONS_PREF_S8_CODING,ESP_BLE_GAP_PHY_OPTIONS_PREF_S8_CODING);
   if (err != ESP_OK) {
-    ESP_LOGE(TAG, "esp_ble_gap_set_prefered_default_phy failed: %d", err);
+    ESP_LOGE(TAG, "esp_ble_gap_set_preferred_default_phy failed: %d", err);
     return false;
   }
   
