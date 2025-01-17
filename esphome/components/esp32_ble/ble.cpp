4f90de6b37ae3988831d86b4887c01ffbe1b5403
Enrico <crs.enrico@gmail.com>
2025-01-17 18:30:43 +0100

    Update ble.cpp

--- esphome/components/esp32_ble/ble.cpp
+++ esphome/components/esp32_ble/ble.cpp
@@ -208,7 +208,7 @@ bool ESP32BLE::ble_setup_() {
     }
   }
  
-  err = esp_ble_gap_set_preferred_default_phy(ESP_BLE_GAP_PHY_CODED_PREF_MASK,ESP_BLE_GAP_PHY_CODED_PREF_MASK);
+  err = esp_ble_gap_set_preferred_default_phy(ESP_BLE_GAP_PHY_CODED,ESP_BLE_GAP_PHY_CODED);
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "esp_ble_gap_set_preferred_default_phy failed: %d", err);
     return false;
